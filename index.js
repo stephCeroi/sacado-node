76959624f1f8c6eef7383b12624668d8ac326276
stephCeroi <82594360+stephCeroi@users.noreply.github.com>
2023-10-28 09:42:46 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+/* Création d'une route. Chaque route correspondra à un URL différent. 
+   Ici cela correspond à http://mon_adresse/
+   Si nous avions  app.get('/name', function (req, res) {
+   La route serait http://mon_adresse/name 
+*/
+app.get('/', function (req, res) {
+  res.send('Hello World, le retour');
+});
+
+app.get('/toto', function(req,res) {
+    res.send("deuxieme page");
+});
+
+// On écoute le port 3000
+app.listen(8082, function () {
+  console.log('Example app listening on port 8082!');
+});
